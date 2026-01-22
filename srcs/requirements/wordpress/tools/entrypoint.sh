bb33abae740161178dd08624710e0031d2c0143a
mehdi <elmehdihafiane@gmail.com>
2026-01-22 17:11:48 +0100

    all

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -19,7 +19,7 @@ if [ ! -f wp-config.php ]; then
     wp core download --allow-root
     wp config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$DB_PASSWORD --dbhost=$MARIADB_HOST --allow-root
     wp core install --url=https://$DOMAIN_NAME --title=$WP_SITE_TITLE --admin_user=$WP_ADMIN_USER --admin_email=$WP_ADMIN_EMAIL --admin_password=$ADMIN_PASSWORD --skip-email --allow-root
-    wp user create $WP_AUTHOR_USER $WP_AUTHOR_EMAIL --role=author --user_pass=$AUTHOR_PASSWORD --allow-root || true
+    wp user create $WP_AUTHOR_USER $WP_AUTHOR_EMAIL --role=author --user_pass=$AUTHOR_PASSWORD --allow-root
 fi
 
 exec "$@"
